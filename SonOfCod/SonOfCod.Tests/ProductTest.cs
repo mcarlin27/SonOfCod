7877a7baa17a0725c15c5d03b8e80da72499bbb0
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-08-11 11:22:12 -0700

    add initial testing project; add first test on Product class; test fails as expected (red)

---
+++ SonOfCod/SonOfCod.Tests/ProductTest.cs
@@ -0,0 +1,21 @@
+﻿using SonOfCod.Models;
+using Xunit;
+
+namespace SonOfCod.Tests
+{
+    public class ItemTest
+    {
+        [Fact]
+        public void GetDescriptionTest()
+        {
+            //Arrange
+            var product = new Product();
+
+            //Act
+            var result = product.Name;
+
+            //Assert
+            Assert.Equal("Alaska Pollock", result);
+        }
+    }
+}
---
+++ SonOfCod/SonOfCod.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SonOfCod.Tests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7f147c2e-e41d-4bb0-9a64-f14be8a5e508")]
